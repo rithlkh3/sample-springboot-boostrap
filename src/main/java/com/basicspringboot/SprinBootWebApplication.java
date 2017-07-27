34e293338cf26c91c06a3f69ed65381bb86ea885
rithlkh3 <r.ron@gl-f.com>
2017-07-27 12:58:01 +0700

    full sample spring boot with bootstrap

---
+++ src/main/java/com/basicspringboot/SprinBootWebApplication.java
@@ -0,0 +1,18 @@
+package com.basicspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Copyright (c) GL Finance Plc. All rights reserved. (http://www.gl-f.com/)
+ * Author: Ron Rith (r.ron@gl-f.com) on 2017-07-27.
+ */
+@SpringBootApplication
+public class SprinBootWebApplication {
+    public static void main(String[] args){
+        SpringApplication.run(SprinBootWebApplication.class,args);
+    }
+}
+
+
+// SpringBoot start auto configure and scan all component
---
+++ src/main/java/com/basicspringboot/WelcomeController.java
@@ -0,0 +1,26 @@
+package com.basicspringboot;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Map;
+
+/**
+ * Copyright (c) GL Finance Plc. All rights reserved. (http://www.gl-f.com/)
+ * Author: Ron Rith (r.ron@gl-f.com) on 2017-07-27.
+ */
+@Controller
+public class WelcomeController {
+
+    //inject var application.properties
+    @Value("${welcome.message:default vaule while first variable not defined}")
+    private String message = "Hello World!";
+
+    @RequestMapping("/")
+    public String welcome(Map<String,Object> model){
+        model.put("message",this.message);
+        return "welcome";
+    }
+
+}
